edbac49b1cbaeb3752197aff15146674ac98fce2
spabwoodle <rob.cusack@hotmail.co.uk>
2020-06-13 18:22:21 +0100

    Add files via upload

---
+++ coffee2.py
@@ -0,0 +1,36 @@
+water = int(input("Write how many ml of water the coffee machine has: "))
+milk = int(input("Write how many ml of milk the coffee machine has: "))
+coffee_beans = int(input("Write how many grams of coffee beans the coffee machine has: "))
+cups_coffee = int(input("Write how many cups of coffee you will need: "))
+
+def cup_quantifier(ingredient, divider):
+    number_of_cups = ingredient / divider
+    return int(number_of_cups)
+
+sufficient_water = water >= (cups_coffee * 200)
+sufficient_milk = milk >= (cups_coffee * 50)
+sufficient_coffee = coffee_beans >= (cups_coffee * 15) 
+
+available_water = cup_quantifier(water, 200)
+available_milk = cup_quantifier(milk, 50)
+available_beans = cup_quantifier(coffee_beans, 15)
+
+def lowest_number(a, b, c):
+    if a <= b and a <= c:
+        return a
+    elif b <= a and b <= c:
+        return b
+    else:
+        return c 
+
+def coffee_machine(water, milk, coffee_beans, cups_coffee):
+    if sufficient_water is False or sufficient_milk is False or sufficient_coffee is False:
+        number_of_cups = lowest_number(available_water, available_milk, available_beans)
+        return("No, I can make only " + str(number_of_cups) + " cups of coffee")      
+    elif available_water >= 2 and available_milk >= 2 and available_beans >= 2:
+        number_of_cups = lowest_number(available_water, available_milk, available_beans) - cups_coffee          
+        return("Yes, I can make that amount of coffee (and even " + str(number_of_cups) + " more than that)")
+    else:
+        return("Yes, I can make that amount of coffee")
+
+print(coffee_machine(water, milk, coffee_beans, cups_coffee))
