3b80735e6fe0dfcb63fb4b2b70392421aad51b0b
spabwoodle <rob.cusack@hotmail.co.uk>
2020-06-17 14:31:36 +0100

    Update coffee4.py

--- coffee4.py
+++ coffee4.py
@@ -1,19 +1,16 @@
 # Updates coffee3.py, asks user if they want to buy coffee, fill the machine, 
-# take cash, ask for levels or exit. 
-# Runs on loop and updates levels throughout.
+# take cash, ask for levels or exit. Runs on loop and updates levels throughout.
 
-#quantities available at start
-cash_float = 550 #dollars
-water = 400 #millilitres
-milk = 540 #millilitres
-coffee_beans = 120 #grams
-disposable_cups = 9
+# Coffee machine contents at start:
+# [dollars cash, ml water, ml milk, grams coffee beans, disposable cups]
+contents = [550, 400, 540, 120, 9] 
 
 #quantities per drink (price, ml water, ml milk, grams coffee)
 espresso = [4, 250, 0, 16]
 latte = [7, 350, 75, 20]
 cappuccino = [6, 200, 100, 12]
 hot_drink = []
+
 
 def coffee_machine_status(water, milk, coffee_beans, disposable_cups, cash):    
     print("""\nThe coffee machine has:
@@ -22,28 +19,24 @@ def coffee_machine_status(water, milk, coffee_beans, disposable_cups, cash):
 %d of coffee beans
 %d of disposable cups
 $%d of money""" % 
-    (water, milk, coffee_beans, disposable_cups, cash))
+    (contents[1], contents[2], contents[3], contents[4], contents[0]))
     
+
 def update_stock(water_needed, milk_needed, coffee_needed, cash_needed):
-    global water
-    global milk
-    global coffee_beans 
-    global disposable_cups
-    global cash_float
-    if (water < water_needed):
+    if (contents[1] < water_needed):
         print("Sorry, not enough water!")
     else:
-        if milk < milk_needed:
+        if contents[2] < milk_needed:
             print("Sorry, not enough milk!\n")
         else:
-            if coffee_beans < coffee_needed:
+            if contents[3] < coffee_needed:
                 print("Sorry, not enough coffee!\n")
             else:
-                water = water - water_needed
-                milk = milk - milk_needed
-                coffee_beans = coffee_beans - coffee_needed
-                disposable_cups = disposable_cups - 1
-                cash_float = cash_float + cash_needed
+                contents[1] = contents[1] - water_needed
+                contents[2] = contents[2] - milk_needed
+                contents[3] = contents[3] - coffee_needed
+                contents[4] = contents[4] - 1
+                contents[0] = contents[0] + cash_needed
                 print("I have enough resources, making you a coffee!")
 
 
@@ -58,36 +51,34 @@ def coffee_machine_buy():
     elif buy_request == "back":
         pass
 
+
 def coffee_machine_fill():
     water_request = int(input("\nWrite how many ml of water do you want to add:\n"))
     milk_request = int(input("Write how many ml of milk do you want to add:\n"))
     coffee_request = int(input("Write how many grams of coffee beans do you want to add:\n"))
     cup_request = int(input("Write how many disposable cups of coffee do you want to add:\n"))
     
-    global water
-    global milk
-    global coffee_beans 
-    global disposable_cups
-    global cash_float
-    water = water + water_request
-    milk = milk + milk_request
-    coffee_beans = coffee_beans + coffee_request
-    disposable_cups = disposable_cups + cup_request
+    contents[1] = contents[1] + water_request
+    contents[2] = contents[2] + milk_request
+    contents[3] = contents[3] + coffee_request
+    contents[4] = contents[4] + cup_request
+
 
 def coffee_machine_take():
-    global cash_float
-    print("\nI gave you $%d" % (cash_float))
-    cash_float = 0
+    global contents
+    print("\nI gave you $%d" % (contents[0]))
+    contents[0] = 0
+
 
 def coffee_machine_request():
         if request == "fill":
             coffee_machine_fill()
-        if request == "buy":
+        elif request == "buy":
             coffee_machine_buy()
         elif request == "take":
             coffee_machine_take()
         elif request == "remaining":
-            coffee_machine_status(water, milk, coffee_beans, disposable_cups, cash_float)
+            coffee_machine_status(contents[1], contents[2], contents[3], contents[4], contents[0])
 
 #start of program:
 
